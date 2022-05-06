e141cdf272d3c16bc913189fde391834b80bf092
Douglas Henrique <dev.dhbarboza@gmail.com>
2022-05-05 22:40:42 -0300

    Add component

---
+++ frontend/src/components/WidgetForm.tsx
@@ -0,0 +1,17 @@
+export function WidgetForm () {
+  return(
+    <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
+      <header>
+        <span className="text-xl leading-6">Contribua com seu fedback</span>
+      </header>
+
+      <main>
+
+      </main>
+
+      <footer className="text-xs text-neutral-400">
+        Projeto ♥ <a className="underline underline-offset-2" href="https://github.com/DhBarboza">GitHub</a>
+      </footer>
+    </div>
+  )
+}
